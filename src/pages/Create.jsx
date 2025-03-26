4171b62705acae6d65fa7e77f8a3017c3052a404
Aditya Singh <adityaa9170843@gmail.com>
2025-03-26 22:18:51 +0530

    added new component

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
-
+import React from "react";
+import { Form } from "react-router-dom";
+import {
+  Button,
+  Field,
+  Fieldset,
+  For,
+  Input,
+  NativeSelect,
+  Stack,
+  Box,
+  Textarea,
+  Checkbox,
+} from "@chakra-ui/react";
 function Create() {
   return (
-    <div>Create</div>
-  )
+    <Box maxW="480px" p={"20px"} >
+      <Form method="post" action="/create" >
+        <Fieldset.Root size="lg" maxW="md">
+          <Stack>
+            <Fieldset.Legend>Contact details</Fieldset.Legend>
+            <Fieldset.HelperText>
+              Please provide your contact details below.
+            </Fieldset.HelperText>
+          </Stack>
+
+          <Fieldset.Content>
+            <Field.Root>
+              <Field.Label>Name</Field.Label>
+              <Input name="name" />
+              <Fieldset.HelperText>Enter your name above</Fieldset.HelperText>
+            </Field.Root>
+
+            <Field.Root>
+              <Field.Label>Task Description</Field.Label>
+              <Textarea
+                placeholder="Enter your task description here"
+                name="description"
+              />
+            </Field.Root>
+
+            <Field.Root>
+              <Checkbox.Root colorPalette={"red"} size={"sm"} name="isUrgent">
+                <Checkbox.HiddenInput />
+                <Checkbox.Control mr={2} />
+                <Checkbox.Label>Urgent?</Checkbox.Label>
+              </Checkbox.Root>
+            </Field.Root>
+
+            <Field.Root>
+              <Field.Label>Country</Field.Label>
+              <NativeSelect.Root>
+                <NativeSelect.Field name="country">
+                  <For each={["United Kingdom", "Canada", "United States"]}>
+                    {(item) => (
+                      <option key={item} value={item}>
+                        {item}
+                      </option>
+                    )}
+                  </For>
+                </NativeSelect.Field>
+                <NativeSelect.Indicator />
+              </NativeSelect.Root>
+            </Field.Root>
+          </Fieldset.Content>
+
+          <Button type="submit" colorPalette={"purple"} alignSelf="flex-start">
+            Submit
+          </Button>
+        </Fieldset.Root>
+      </Form>
+    </Box>
+  );
 }
 
-export default Create
+export default Create;
+
+export const createAction = async ({ request }) => {
+  const data = await request.formData();
+
+  const task = {
+    title: data.get("name"),
+    description: data.get("description"),
+    isUrgent: data.get("isUrgent") === "",
+  };
+  }
+
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { List } from "@chakra-ui/react";
+import { Icon, Tabs } from "@chakra-ui/react";
+import React from "react";
+import { HiAcademicCap, HiBadgeCheck } from "react-icons/hi";
+import { HiBanknotes } from "react-icons/hi2";
+
+
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <Tabs.Root
+      mt="40px"
+      p="20px"
+      colorPalette={"purple"}
+      defaultValue="tab-1"
+      variant={"enclosed"}
+    >
+      <Tabs.List>
+        <Tabs.Trigger
+          value="tab-1"
+          _selected={{ colorPalette: "red", bg: "cyan.100" }}
+        >
+          Account Info
+        </Tabs.Trigger>
+        <Tabs.Trigger value="tab-2">Tasks</Tabs.Trigger>
+        <Tabs.Trigger value="tab-3">Advanced Tasks</Tabs.Trigger>
+      </Tabs.List>
+
+      <Tabs.Content value="tab-1">
+        <List.Root gap={4} listStyle={"none"}>
+          <List.Item >
+            <Icon as={HiAcademicCap} mr={3} />
+            Email:aditya@ninja.slash
+          </List.Item>
+          <List.Item>
+            <Icon as={HiBanknotes} mr={3}  />
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, rem?
+          </List.Item>
+          <List.Item>
+            <Icon as={HiBadgeCheck} mr={3} />
+            yes
+          </List.Item>
+        </List.Root>
+      </Tabs.Content>
+
+      <Tabs.Content value="tab-2">Tasks</Tabs.Content>
+
+      <Tabs.Content value="tab-3">Advanced Tasks</Tabs.Content>
+    </Tabs.Root>
+  );
 }
 
-export default Profile
+export default Profile;
